f382b9d2ae32e4167da703834cecbd23366571be
utsav1213 <hiraprautsav54@gmail.com>
2025-02-11 14:13:02 +0530

    Task 3 is added

---
+++ Task 3/src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from '../../Task 3/src/components/Footer'
+import Header from '../../Task 3/src/components/Header'
+import MainContent from '../../Task 3/src/components/MainContent'
+function App() {
+  
+
+  return (
+    <>
+   <Header/>
+   <MainContent/>
+   <Footer/>
+     </>
+  )
+}
+
+export default App
